26c64bafc0a019545f740a993bcf3af084fc2bd5
diojer <78817584+diojer@users.noreply.github.com>
2022-03-03 22:56:29 +0000

    omnidirectional walking

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,32 +1,45 @@
 extends KinematicBody2D
 
-export var MOVE_SPEED = 10000
-var motion = Vector2()
-
-func cartesian_to_isometric(cartesian):
-	#var screen_pos = Vector2()
-	#screen_pos.x = cartesian.x - cartesian.y
-	#screen_pos.y = (cartesian.x + cartesian.y) / 2
-	#return screen_pos
-	#above algorithm is needed if we want to railroad players into isometric movement,
-	#but i don't think it's necessary since we're using 8 cardinal directions.
-	return(Vector2(cartesian.x, cartesian.y / 2)) #dividing the y value by two makes the character run parallel to isometric lines
+export var MOVE_SPEED = 200
+var direction = Vector2(0, 0)
 func _ready():
-	pass # Replace with function body.
+	pass
 func _physics_process(delta):
-	var direction = Vector2()
-
-	if Input.is_action_pressed("move_n"):
-		direction += Vector2(0, -1)
-	elif Input.is_action_pressed("move_s"):
-		direction += Vector2(0, 1)
-
-	if Input.is_action_pressed("move_w"):
-		direction += Vector2(-1, 0)
-	elif Input.is_action_pressed("move_e"):
-		direction += Vector2(1, 0)
-	motion = direction.normalized() * MOVE_SPEED * delta
-	motion = cartesian_to_isometric(motion)
-	move_and_slide(motion)
+	MovementLoop()
 	
+func _process(delta):
+	AnimationLoop()
+	
+func MovementLoop():
+	direction.x = int(Input.is_action_pressed("move_e")) - int(Input.is_action_pressed("move_w"))
+	direction.y = (int(Input.is_action_pressed("move_s")) - int(Input.is_action_pressed("move_n"))) / float(2)
+	var motion = direction.normalized() * MOVE_SPEED
+	move_and_slide(motion)
 
+func AnimationLoop():
+	var anim_direction = "S"
+	var anim_mode = "Idle"
+	var animation
+	match direction:
+		Vector2(-1, 0):
+			anim_direction = "W"
+		Vector2(1, 0):
+			anim_direction = "E"
+		Vector2(0, -0.5):
+			anim_direction = "N"
+		Vector2(0, 0.5):
+			anim_direction = "S"
+		Vector2(-1, -0.5):
+			anim_direction = "NW"
+		Vector2(-1, 0.5):
+			anim_direction = "SW"
+		Vector2(1, -0.5):
+			anim_direction = "NE"
+		Vector2(1, 0.5):
+			anim_direction = "SE"
+	if direction != Vector2(0, 0):
+		anim_mode = "Idle"
+	else:
+		anim_mode = "Idle"
+	animation = anim_mode + "_" + anim_direction
+	get_node("AnimationPlayer").play(animation)
